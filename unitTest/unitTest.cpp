684881e2c30e02844ef5d936b3bcb7d8ce2b30bc
Rupianchyn-Oleh <rypianchyn.oleh.in@dspu.edu.ua>
2025-11-23 15:22:03 +0200

    main

---
+++ unitTest/unitTest.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../PR50/PR50.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace unitTest
+{
+	TEST_CLASS(unitTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int t;
+			t = sum(2, 3);
+			Assert::AreEqual(t, 5);
+		}
+	};
+}
